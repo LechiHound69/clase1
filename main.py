618b0b1a52ff268a026126defef0dd50e974e7e4
LechiHound69 <cristobalmejiam@gmail.com>
2026-02-06 19:18:58 -0500

    feat: cambiado a factorial

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-def sumar(num:int) -> int:
-    return num + 1
+def factorial(num: int) -> int:
+    """
+    Factorial
 
-print(sumar(5))
+    Una función para calcular el factorial de un número entero ingresado
+    :type num: int
+    :return: El factorial calculado del número num ingresado
+    :rtype: int
+    """
+    if num == 1:
+        return num
+    else:
+        fact = factorial(num - 1)
+        fact *= num
+        return fact
+
+
+print(factorial(5))
